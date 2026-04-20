165daaaca78cd66e0f9007819f368e723be3f2f6
YefriLermaLP <yefrilp.dev@gmail.com>
2026-04-19 20:24:00 -0500

    tarea3

--- ms-catalogo/src/main/java/com/example/mscatalogo/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mscatalogo.repository;
-
-import com.example.mscatalogo.entity.Categoria;
-import com.example.mscatalogo.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
